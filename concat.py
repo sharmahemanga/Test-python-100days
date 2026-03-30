12cb0eb750ac93b7af11ceb91855487da8c96df2
sharmahemanga <45197190+sharmahemanga@users.noreply.github.com>
2026-03-30 23:58:01 +0530

    This code is to check how to concatenate input prompt inside a print statement

---
+++ concat.py
@@ -0,0 +1,6 @@
+# This code is to check how to concatenate input prompt inside a print statement
+print("Hello" + input("what is your name?")+"!")
+
+# This is another option
+a = input("what is your name? ")
+print(f"hello {a}!")
