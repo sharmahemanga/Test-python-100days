91016fef53baf09451baddcf4c5cb0980cee0239
sharmahemanga <45197190+sharmahemanga@users.noreply.github.com>
2026-03-30 23:44:56 +0530

    This is a test code to test the capabilities of the input function

---
+++ input.py
@@ -0,0 +1,5 @@
+# This is a test code to test the capabilities of the input function
+# Also I am trying to check how f string can be used and how to concatenate two variables within a print statement
+a = input("what is your name? ")
+b = input("what is the surname? ")
+print(f"My full name is - {a} {b}")
